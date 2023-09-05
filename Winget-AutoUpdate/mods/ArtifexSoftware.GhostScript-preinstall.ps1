318c14a4c7e734b07325f932f8e9e9d23bc00974
IT User <82225899+user1722@users.noreply.github.com>
2023-09-05 14:42:13 +0200

    Update ArtifexSoftware.GhostScript-preinstall.ps1

--- Winget-AutoUpdate/mods/ArtifexSoftware.GhostScript-preinstall.ps1
+++ Winget-AutoUpdate/mods/ArtifexSoftware.GhostScript-preinstall.ps1
@@ -167,16 +167,6 @@ function Stop-ModsProc ($Proc) {
 	}
 
 
-function Stop-ModsProc ($Proc) {
-    foreach ($process in $Proc)
-    {
-        Stop-Process -Name $process -Force -ErrorAction SilentlyContinue | Out-Null
-    }
-    Return
-	}
-
-
-
 if ($Proc) {
     Stop-ModsProc $Proc
     }
