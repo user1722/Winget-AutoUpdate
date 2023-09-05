2ee28cdf63e47df01373983b1ce7fb183d187604
IT User <82225899+user1722@users.noreply.github.com>
2023-09-05 14:52:04 +0200

    Update geeksoftwareGmbH.PDF24Creator-preinstall.ps1

--- Winget-AutoUpdate/mods/geeksoftwareGmbH.PDF24Creator-preinstall.ps1
+++ Winget-AutoUpdate/mods/geeksoftwareGmbH.PDF24Creator-preinstall.ps1
@@ -166,17 +166,6 @@ function Stop-ModsProc ($Proc) {
     Return
 	}
 
-
-function Stop-ModsProc ($Proc) {
-    foreach ($process in $Proc)
-    {
-        Stop-Process -Name $process -Force -ErrorAction SilentlyContinue | Out-Null
-    }
-    Return
-	}
-
-
-
 if ($Proc) {
     Stop-ModsProc $Proc
     }
