3efb1645a118fbeb354b556f8a40c31cf59f79af
IT User <82225899+user1722@users.noreply.github.com>
2023-09-07 14:09:22 +0200

    Update Microsoft.VCRedist.2012.x64-preinstall.ps1

--- Winget-AutoUpdate/mods/Microsoft.VCRedist.2012.x64-preinstall.ps1
+++ Winget-AutoUpdate/mods/Microsoft.VCRedist.2012.x64-preinstall.ps1
@@ -1,6 +1,6 @@
 ﻿#Beginning of App Name string to Silently Uninstall (MSI/NSIS/INNO/EXE with defined silent uninstall in registry)
 #Multiple: "app1*","app2*", required wildcard (*) after; search is done with "-like"!
-$App = @("Microsoft Visual C++ 2012 Redistributabl*")
+$App = @("Microsoft Visual C++ 2012 Redistributabl*","Microsoft Visual C++ 2012 Redistributabl*","Microsoft Visual C++ 2012 Redistributabl*")
 
 $Proc = @("")
 
