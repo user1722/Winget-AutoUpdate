81da359f1cae2895221a6b05e143b91039e17785
IT User <82225899+user1722@users.noreply.github.com>
2023-09-22 11:03:06 +0200

    Update Get-WingetCmd.ps1

--- Winget-AutoUpdate/functions/Get-WingetCmd.ps1
+++ Winget-AutoUpdate/functions/Get-WingetCmd.ps1
@@ -1,37 +1,53 @@
-#Function to get the winget command regarding execution context (User, System...)
+# Function to get the winget command regarding execution context (User, System...)
 
-Function Get-WingetCmd {
+function Get-WingetCmd
+{
 
-    #Get WinGet Path (if admin context)
-    # Includes Workaround for ARM64 (removed X64 and replaces it with a wildcard)
-    $ResolveWingetPath = Resolve-Path "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_*__8wekyb3d8bbwe" | Sort-Object { [version]($_.Path -replace '^[^\d]+_((\d+\.)*\d+)_.*', '$1') }
+   # Get WinGet Path (if Admin context)
+   # Includes Workaround for ARM64 (removed X64 and replaces it with a wildcard)
+   $ResolveWingetPath = (Resolve-Path -Path "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_*__8wekyb3d8bbwe" | Sort-Object -Property {
+         [version]($_.Path -replace '^[^\d]+_((\d+\.)*\d+)_.*', '$1')
+      })
 
-    if ($ResolveWingetPath) {
-        #If multiple version, pick last one
-        $WingetPath = $ResolveWingetPath[-1].Path
-    }
+   if ($ResolveWingetPath)
+   {
+      # If multiple version, pick last one
+      $WingetPath = $ResolveWingetPath[-1].Path
+   }
 
-    #Get Winget Location in User context
-    $WingetCmd = Get-Command winget.exe -ErrorAction SilentlyContinue
-    if ($WingetCmd) {
-        $Script:Winget = $WingetCmd.Source
-    }
-    #Get Winget Location in System context
-    elseif (Test-Path "$WingetPath\winget.exe") {
-        $Script:Winget = "$WingetPath\winget.exe"
-    }
-    else {
-        Write-ToLog "Winget not installed or detected !" "Red"
-        return $false
-    }
+   #If running under System or Admin context obtain Winget from Program Files
+   if ((([Security.Principal.WindowsIdentity]::GetCurrent().User) -eq 'S-1-5-18') -or ($WingetPath))
+   {
+      if (Test-Path -Path ('{0}\winget.exe' -f $WingetPath) -ErrorAction SilentlyContinue)
+      {
+         $Script:Winget = ('{0}\winget.exe' -f $WingetPath)
+      }
+   }
+   else
+   {
+      #Get Winget Location in User context
+      $WingetCmd = (Get-Command -Name winget.exe -ErrorAction SilentlyContinue)
 
-    #Run winget to list apps and accept source agrements (necessary on first run)
-    & $Winget list --accept-source-agreements -s winget | Out-Null
+      if ($WingetCmd)
+      {
+         $Script:Winget = $WingetCmd.Source
+      }
+   }
 
-    #Log Winget installed version
-    $WingetVer = & $Winget --version
-    Write-ToLog "Winget Version: $WingetVer"
+   if (!($Script:Winget))
+   {
+      Write-ToLog 'Winget not installed or detected !' 'Red'
 
-    return $true
+      return $false
+   }
+
+   # Run winget to list apps and accept source agrements (necessary on first run)
+   $null = (& $Winget list --accept-source-agreements -s winget)
+
+   # Log Winget installed version
+   $WingetVer = & $Winget --version
+   Write-ToLog ('Winget Version: {0}' -f $WingetVer)
+
+   return $true
 
 }
